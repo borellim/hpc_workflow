ca1be555609b7c9452ddecc94b8769d2d04a3965
Marco Borelli <marco.borelli@epfl.ch>
2018-01-16 11:32:32 +0100

    Marco changes

--- submit/script.sh
+++ submit/script.sh
@@ -4,8 +4,8 @@ hpcfile_sirius=hpc-sirius.json
 pwfile=pw.json
 dirname=$(awk 'NR==2 {print $3}' ${hpcfile}| grep -o '".*"'| sed 's/"//g')
 scale=$(grep scale ${hpcfile} |awk '{print $3}')
-a=$(verdi run submit_benchmarks.py  pw-6.1-sirius@PizDaintGPU  ${hpcfile_sirius} ${pwfile} )
-b=$(verdi run submit_benchmarks.py  pw-6.1@fidis ${hpcfile} ${pwfile})
+a=$(verdi run submit_benchmarks.py  QE62_SIRIUS_Marco12@daint5  ${hpcfile_sirius} ${pwfile} )
+#b=$(verdi run submit_benchmarks.py  pw-6.1@fidis ${hpcfile} ${pwfile})
 c=$(date +%F-%k-%m-%S)
 echo $a $b  > workflow_list
 mkdir -p ${dirname}${scale}
